813c85b3caf2c3999b534c67e68408fcd7562fc9
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-11-14 23:12:01 +0300

    update on headers vector.h & vector2d.h

---
+++ src/include/vector.h
@@ -0,0 +1,65 @@
+/**
+ * @file vector.h
+ *
+ * Header file contain class vector,
+ * which is a based class for the
+ * class vector2d contain basic math
+ * for vector algebra in 2D.
+ *
+ */
+
+#ifndef _VECTOR_H_
+#define _VECTOR_H_
+
+//! vector basic definition
+#include <ostream>
+
+//! Generic class definition
+template <class Gtype>
+class vector
+{
+  friend std::ostream& operator<<(std::ostream& os, const vector& v)
+  {
+    if (v.verbose)
+      os << "Vector:[" << v.id << "] " << v.get_x() << "i+" << v.get_y() << "j";
+    else
+      os << "Vector:[" << v.id << "] <" << v.get_x() << "," << v.get_y() << ">";
+    return os;
+  }
+
+ public:
+  //! this is the constructors definition of class vector
+  vector();
+  vector(Gtype, Gtype);
+
+  //! this is the destructor definition of class vector
+  ~vector();
+
+  //! this is the public getters methods for x and y
+  Gtype get_x() const;
+  Gtype get_y() const;
+  //! this is the public getters methods for id & counter
+  int get_counter() const;
+  int get_id() const;
+  //! this is the test method to check the verbose of object
+  bool is_active() const;
+
+  //! this is the public setters methods for x and y
+  void set_x(Gtype);
+  void set_y(Gtype);
+  //! this is the setters method for object id#
+  void set_id(int);
+  //! this is the setters method for object verbose state
+  void set_active(bool);
+
+  // this is the definition of virtual display method
+  virtual void display() const;
+
+ protected:
+  Gtype x{};            // x coordinate of the vector
+  Gtype y{};            // y coordinate of the vector
+  static int counter;   // Created Objects Counter
+  int id{0};            // created object ID#
+  bool verbose{false};  // object verbose state active or inactive
+};
+#endif  // !_VECTOR_H_
--- src/include/vector2d.h
+++ src/include/vector2d.h
@@ -1,33 +1,43 @@
-//////////////////////////////////////
-// vector2d.h header file is a part //
-// of the graphic engine by use the //
-//  SDL library and cpp. this file  //
-//  will inherit from the vect.h    //
-// templated type class can accepet //
-// various type of primitive data.  //
-//////////////////////////////////////
+/**
+ *
+ * @file vector2d.h
+ * this header file is contain basic
+ * math algebra of vector in 2d,
+ * which is part of the graphic engine
+ * this file contain class named vector2d
+ * will inherit the class vector from
+ * the vector.h
+ * it's templated type class can accept
+ * various type of primitive data.
+ *
+ */
 
 #ifndef _VECTOR_2D_H_
 #define _VECTOR_2D_H_
 // the base class for the vector2d header file.
-#include "vect.h"
-// using of templated type class
-template <class Gtype>
-class vector2d : public vect<Gtype> {
-public:
-    // vector2d class constructor
+#include "vector.h"
+
+namespace immutable
+{
+
+  //! using of templated type class
+  template <class Gtype>
+  class vector2d : public vector<Gtype>
+  {  // class immutable::vector2d inherit from class vector
+   public:
+    //! this is vector2d class constructor definition
     vector2d();
     vector2d(Gtype, Gtype);
-    // vector2d class destructor
+    //! this is vector2d class destructor definition
     ~vector2d();
-    // vector2d class copy constructor
+    // this is vector2d class copy constructor definition
     vector2d(const vector2d&);
 
-    // class operator overload
+    // this is class operators overload definition
     vector2d operator+(vector2d&);
     vector2d operator()(Gtype, Gtype);
 
-    // class methods for vector 2D math
+    // this is class methods for vector 2D math
     vector2d add(const vector2d&);
     vector2d sub(const vector2d&);
     vector2d multi(const Gtype&);
@@ -35,7 +45,8 @@ public:
     vector2d divide(const Gtype&);
     Gtype dot(const vector2d&);
 
-private:
-    // int id{};  // created objects ID#
-};
+   private:
+    //! code here
+  };
+}  // namespace immutable
 #endif  // _VECTOR_2D_H_
