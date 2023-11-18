b1f343aa8d56644b319b2a8e6c95ec7637779446
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-11-18 22:41:46 +0300

    add the operators overload methods

--- src/lib/vector2d.cpp
+++ src/lib/vector2d.cpp
@@ -4,36 +4,90 @@
 
 #include "vector.h"
 
+//! This is the class immutable vector in 2d space, contain basic vector algebra
 namespace immutable
 {
 
-  //! this is implementation of class constructor
+  //! this is default class constructor implementation, which create object of
+  //! type vector (0,0)
   template <class Gtype>
   vector2d<Gtype>::vector2d() : vector<Gtype>()
   {
   }
-
+  //! this is the class constructor take x,y of generic type and create object
+  //! of type vector (x,y)
   template <class Gtype>
   vector2d<Gtype>::vector2d(Gtype x, Gtype y) : vector<Gtype>(x, y)
   {
   }
-  //! this is the implementation of class copy constructor
+  //! this is the implementation of class copy constructor vector(v.x,v.y)
   template <class Gtype>
   vector2d<Gtype>::vector2d(const vector2d<Gtype>& v) : vector<Gtype>(v.x, v.y)
   {
   }
-  //! this is the implementation of class destructor
+  //! this is class vector2d destructor implementation
   template <class Gtype>
   vector2d<Gtype>::~vector2d()
   {
   }
-  //! this is the operators overload implementation
+  //! this is the operator+ overload, which take object by reference added to
+  //! call object (this) and return new vector2d
   template <class Gtype>
   vector2d<Gtype> vector2d<Gtype>::operator+(vector2d<Gtype>& v)
   {
     return vector2d<Gtype>(this->x + v.x, this->y + v.y);
   }
-
+  //! this is the operator- overload, which take object by reference and
+  //! subtract it from (this) and return new vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator-(vector2d<Gtype>& v)
+  {
+    return vector2d<Gtype>(this->x - v.x, this->y - v.y);
+  }
+  //! this is the operator* overload, which take var with Gtype and return
+  //! object of type vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator*(Gtype& scalier)
+  {
+    return vector2d<Gtype>(this->x * scalier, this->y * scalier);
+  }
+  //! this is the operator/ overload, which take var of type Gtype and return a
+  //! vector2d object
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator/(Gtype& scalier)
+  {
+    return vector2d<Gtype>(this->x / scalier, this->y / scalier);
+  }
+  //! this is the operator+= overload, which take object of type vector2d added
+  //! to (this) and return new vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator+=(vector2d<Gtype>& v)
+  {
+    return vector2d<Gtype>(this->x + v.x, this->y + v.y);
+  }
+  //! this is the operator-= overload, which take object of type vector2d
+  //! subtract it from (this) and return new vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator-=(vector2d<Gtype>& v)
+  {
+    return vector2d<Gtype>(this->x - v.x, this->y - v.y);
+  }
+  //! this is the operator*= overload, which take var of type Gtype and return
+  //! an object of vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator*=(Gtype& scalier)
+  {
+    return vector2d<Gtype>(this->x * scalier, this->y * scalier);
+  }
+  //! this is the operator/= overload, which take var of type Gtype and return
+  //! object of type vector2d
+  template <class Gtype>
+  vector2d<Gtype> vector2d<Gtype>::operator/=(Gtype& scalier)
+  {
+    return vector2d<Gtype>(this->x / scalier, this->y / scalier);
+  }
+  //! this is the operator() overload, which will make the object created like a
+  //! function and update the caller object (this)
   template <class Gtype>
   vector2d<Gtype> vector2d<Gtype>::operator()(Gtype x_new, Gtype y_new)
   {
