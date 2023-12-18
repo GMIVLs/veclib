11af8bef26d311257420ffdfc61ac556e0d48ce1
eng-alameedi <73557986+eng-alameedi@users.noreply.github.com>
2023-12-19 00:57:14 +0300

    add commits for the log.h file

--- src/include/log.h
+++ src/include/log.h
@@ -1,16 +1,29 @@
+/*********************************
+ * @log.h file is the header     *
+ * file contain the definitation *
+ * of the required methods and   *
+ * fileds of the log class which *
+ * involve about the creating of *
+ * a log function and recording  *
+ * mechanizme and process of log.*
+ * ******************************/
+
 #ifndef _LOG_H_
 #define _LOG_H_
 
+// ! include of the required sytem hearders files
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
+// ! defeniation of the ANIS code for colors
 #define YELLOW "\033[1;33m"
 #define RED "\033[1;31m"
 #define RESET_COLOR "\033[0m"
 #define GREEN "\033[1;32m"
 
+//! the enumaration of the log severity
 enum LogLevel
 {
   TRACE,
